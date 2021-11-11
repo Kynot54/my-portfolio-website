e3164523d5f0733e4ce5f44f4ca00806cf338f0c
Kyle Whynott <kyle.whynott@gmail.com>
2021-11-11 01:20:08 -0800

    am i done yet

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ const cspConfigPolicy = {
     'base-uri': "'self'",
     'object-src': "'none'",
     'script-src': ["'self'"],
-    'style-src': "'self' fonts.googleapis.com",
+    'style-src': "fonts.googleapis.com 'self'",
     'font-src': "fonts.gstatic.com",
 };
 
