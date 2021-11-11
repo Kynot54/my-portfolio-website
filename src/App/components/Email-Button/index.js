5c65536d911f2731f3a9b1931465469451348e85
Kyle Whynott <kyle.whynott@gmail.com>
2021-11-11 14:39:29 -0800

    space in email

--- src/App/components/Email-Button/index.js
+++ src/App/components/Email-Button/index.js
@@ -8,8 +8,7 @@ export default function MyButton() {
         <>
             <Row id="email-container">
                 <Col>
-                    <Button id="email" variant="outline-light" href="mailto:&#107;&#121;&#108;&#101;&#046;&#119;&#104;
-                    &#121;&#110;&#111;&#116;&#116;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+                    <Button id="email" variant="outline-light" href="mailto:&#107;&#121;&#108;&#101;&#046;&#119;&#104;&#121;&#110;&#111;&#116;&#116;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
                      size="lg">Email</Button>
                 </Col>
             </Row>
