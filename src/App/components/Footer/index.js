29e6da80cb0fd6a635cc09373505510979aae1e4
Kynot54 <kyle.whynott@gmail.com>
2022-12-26 21:40:36 -0800

    Removed Unused Components for Build Phase

--- src/App/components/Footer/index.js
+++ src/App/components/Footer/index.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import Button from "react-bootstrap/Button"
 
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
